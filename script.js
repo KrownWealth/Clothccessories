e9980d72cbac6868764430f5715a103de2846546
KrownWealth <abdulramonadeola36@gmail.com>
2023-05-26 16:56:12 +0100

    Made changes to card heaight

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
-// get the product div
-
-const products = document.getElementById("products");
-
-// fetch the products data from storeAPI
+// Fetch the API data and populate the products
 async function fetchData() {
   try {
-    // fetch the data from the api
     const response = await fetch("https://fakestoreapi.com/products");
     const jsonData = await response.json();
-    //add content to products div
-// Loop through the images and create Bootstrap grid items
 
-     {jsonData.map((element) => {
+    const productsContainer = document.getElementById("products");
+
+    const productsHTML = jsonData
+      .map((element) => {
         const description = element.description.slice(0, 80) + "...";
         const title = element.title.slice(0, 20) + "...";
-        const imageCol = document.createElement("div");
-        imageCol.classList.add("col-lg-4", "col-md-6", "col-sm-12",);
-        
 
-        products.innerHTML =`
-      <div class="card">
-              <img src="${element.image}" class="card-img-top" alt="...">
+        return `
+          <div class="col-lg-4 col-md-6 col-sm-12">
+            <div class="card mb-4" style="height: 700px;">
+              <img src="${element.image}" class="card-img-top" style="height: 60%; object-fit: contain;" alt="...">
               <div class="card-body">
                 <h5 class="card-title">${title}</h5>
                 <p class="card-text">${description}</p>
-               
+                <a href="#" class="btn btn-primary">View Product</a>
               </div>
-            </div>`;
-           products.appendChild(imageCol);
-      }).join("")
-    }
-  } catch (err) {
-    console.log(err);
-  }
-   }
+            </div>
+          </div>
+        `;
+      })
+      .join("");
 
+    productsContainer.innerHTML = productsHTML;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 fetchData();
-
-
-
-
-
-
-
-
-
