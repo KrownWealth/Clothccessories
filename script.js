73993008beae3ef963a8f06f561f9389af6435ab
KrownWealth <abdulramonadeola36@gmail.com>
2023-05-31 16:03:07 +0100

    Continuous changes

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 // Fetch the API data and populate the products
 async function fetchData() {
   try {
-    const response = await fetch("https://fakestoreapi.com/products");
+    const response = await fetch("https://fakestoreapi.com/products?limit=8");
     const jsonData = await response.json();
 
     const productsContainer = document.getElementById("products");
 
     const productsHTML = jsonData
       .map((element) => {
-        const description = element.description.slice(0, 80) + "...";
-        const title = element.title.slice(0, 20) + "...";
+        const price = element.price;
+        const title = element.title.slice(0, 10) + "...";
 
         return `
-          <div class="col-lg-4 col-md-6 col-sm-12">
-            <div class="card mb-4" style="height: 700px;">
-              <img src="${element.image}" class="card-img-top" style="height: 60%; object-fit: contain;" alt="...">
-              <div class="card-body">
+          <div class="col-lg-4 col-md-3 col-sm-6">
+            <div class="card mb-4 product-item" style="height: 350px;">
+              <img src="${element.image}" class="card-img-top product-img" style="height: 60%; object-fit: contain;" alt="...">
+              <div class="card-body text-center">
                 <h5 class="card-title">${title}</h5>
-                <p class="card-text">${description}</p>
-                <a href="#" class="btn btn-primary">View Product</a>
+                <p class="card-text"> $ ${price}</p>
+                <button class="btn shop-btn"> <a href="#" class="add-to-cart">Add to Cart </a></button>
+  
               </div>
             </div>
           </div>
